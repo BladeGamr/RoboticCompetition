a2fce53a4e7570be58ec4162a5b81842c1d1135c
Jake3231 <Jake3231@users.noreply.github.com>
2017-11-03 16:05:14 -0500

    Changes
    a;sldkjf

--- Movement.c
+++ Movement.c
@@ -7,41 +7,41 @@
 task main()
 {
 
-while (true) {
+	while (true) {
 
 
-if((vexRT[Btn8L] == 1)) {
-	motor[launchServo] = 127;
-	motor[launchServo] = 127;
-} else {
-	motor[launchServo] = 127;
-	motor[launchServo] = 127;
-}
+		if((vexRT[Btn8L] == 1)) {
+			motor[launchServo] = 127;
+			motor[launchServo] = 127;
+			} else {
+			motor[launchServo] = 127;
+			motor[launchServo] = 127;
+		}
 
-if (abs(vexRT[Ch2]) > 0.5) {
-		motor[Forklift] = (vexRT[Ch2]);
-}
+		if (abs(vexRT[Ch2]) > 0.5) {
+			motor[Forklift] = (vexRT[Ch2]);
+		}
 
 
-// Handle Driving on Left Joystick
-if (abs(vexRT[Ch3]) > abs(vexRT[Ch4])) {
-//Forward Drive
-motor[largeLeft] = vexRT[Ch3];
-motor[largeRight] = vexRT[Ch3];
-} else {
+		// Handle Driving on Left Joystick
+		if (abs(vexRT[Ch3]) > abs(vexRT[Ch4])) {
+			//Forward Drive
+			motor[largeLeft] = vexRT[Ch3];
+			motor[largeRight] = vexRT[Ch3];
+			} else {
 
-if ((vexRT[Ch4]) > 25) {
-// Wide Turn Right
-motor[largeLeft] = vexRT[Ch4];
-motor[largeRight] = vexRT[Ch4] * 0.2;
-}
+			if ((vexRT[Ch4]) > 25) {
+				// Wide Turn Right
+				motor[largeLeft] = vexRT[Ch4];
+				motor[largeRight] = vexRT[Ch4] * 0.2;
+			}
 
-if ((vexRT[Ch4]) < -25) {
-// Wide Turn Left
-motor[largeRight] = vexRT[Ch4] * -1;
-motor[largeLeft] = vexRT[Ch4] * -0.2;
-}
-}
+			if ((vexRT[Ch4]) < -25) {
+				// Wide Turn Left
+				motor[largeRight] = vexRT[Ch4] * -1;
+				motor[largeLeft] = vexRT[Ch4] * -0.2;
+			}
+		}
 
-}
+	}
 }
